c5fb613724e8eafdd48bde73e2d28f70d49cf4c8
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-27 16:03:51 -0400

    added some comments

--- Back-End/db/seeds.rb
+++ Back-End/db/seeds.rb
@@ -16,9 +16,9 @@ wt4 = Top.create(designer: "Nike", name: "4. Black Crop Shirt", score: 1, img_ur
 wt5 = Top.create(designer: "Nike", name: "5. Black/Gray Top", score: 4, img_url: "https://c.static-nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/eflqg50l6mjv5ewrhyg6/pro-womens-cropped-tank-Wh3b5W.jpg")
 
 mt1 = Top.create(designer: "Nike", name: "26. Nike Pro Warm", score: 1, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/qoz4ctxpzofnv9wgvjuj/pro-warm-mens-long-sleeve-top-Dbk0Vt.jpg")
-mt2 = Top.create(designer: "Nike", name: "27. Nike Pro", score: 5, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cqlbhl3d0txkepbdlcpk/pro-mens-tight-fit-long-sleeve-top-N8szFc.jpg")
+mt2 = Top.create(designer: "Nike", name: "27. Nike Pro", score: 2, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cqlbhl3d0txkepbdlcpk/pro-mens-tight-fit-long-sleeve-top-N8szFc.jpg")
 mt3 = Top.create(designer: "Nike", name: "28. Nike Therma", score: 3, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ixdk3bokxq4aduo7eyhb/therma-mens-fleece-pullover-training-hoodie-1LnNMk.jpg")
-mt4 = Top.create(designer: "Nike", name: "29. Nike Therma Repel", score: 2, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/t1bbzz9ri9fk8yjui8p2/therma-repel-mens-1-2-zip-golf-top-hZR66d.jpg")
+mt4 = Top.create(designer: "Nike", name: "29. Nike Therma Repel", score: 5, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/t1bbzz9ri9fk8yjui8p2/therma-repel-mens-1-2-zip-golf-top-hZR66d.jpg")
 mt5 = Top.create(designer: "Nike", name: "30. Nike Sportswear JDI Heavyweight", score: 4, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/mmy6cb2zaysd8r500wyo/sportswear-jdi-heavyweight-fleece-crew-62VKzP.jpg")
 p "Tops were seeded!"
 
@@ -45,22 +45,34 @@ ms1 = Shoe.create(designer: "Nike", name: "26. Air Jordan 3 Retro SE", score: 3,
 ms2 = Shoe.create(designer: "Nike", name: "27. Nike Air Force 1 React", score: 4, img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-e17d75fb-c3e5-4ed4-8a18-16eeac824d73/air-force-1-react-mens-shoe-b7HKxL.jpg")
 ms3 = Shoe.create(designer: "Nike", name: "28. Nike Air Max 90", score: 5, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/wzitsrb4oucx9jukxsmc/air-max-90-mens-shoe-6n3vKB.jpg")
 ms4 = Shoe.create(designer: "Nike", name: "29. Nike Blazer Mid Premium", score: 2, img_url: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-3157c57c-e7f7-47a9-9726-e2a00cdcd35a/blazer-mid-premium-mens-shoe-JWQfgd.jpg")
-ms5 = Shoe.create(designer: "Nike", name: "30. Nike Air Zoom Pegasus 36 FlyEase", score: 0, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/39530e3c-2330-48b9-a7a6-72eb0e8c35ae/air-zoom-pegasus-36-flyease-mens-running-shoe-1Fb6sV.jpg")
+ms5 = Shoe.create(designer: "Nike", name: "30. Nike Air Zoom Pegasus 36 FlyEase", score: 1, img_url: "https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/39530e3c-2330-48b9-a7a6-72eb0e8c35ae/air-zoom-pegasus-36-flyease-mens-running-shoe-1Fb6sV.jpg")
 p "Shoes were seeded!"
 
-o1 = Outfit.create(name: "Basic", season: "Spring", likes: 8, top_id: 8, bottom_id: 5, shoe_id: 5)
-o2 = Outfit.create(name: "Lazy", season: "Fall", likes: 10, top_id: 5, bottom_id: 2, shoe_id: 2)
-o3 = Outfit.create(name: "Athlesure", season: "Summer", likes: 7, top_id: 6, bottom_id: 9, shoe_id: 8)
+o1 = Outfit.create(name: "Basic", season: "Spring", likes: 8, top_id: 10, bottom_id: 3, shoe_id: 6)
+o2 = Outfit.create(name: "Lazy", season: "Fall", likes: 10, top_id: 3, bottom_id: 9, shoe_id: 4)
+o3 = Outfit.create(name: "Athlesure", season: "Summer", likes: 7, top_id: 6, bottom_id: 2, shoe_id: 2)
 p "Outfits were seeded"
 
-c1 = Comment.create(name:"Sam", text: "wutttt", outfit_id: 1)
+c0 = Comment.create(name:"Sam", text: "wutttt", outfit_id: 1)
+c1 = Comment.create(name:"Komal", text: "I'd wear this", outfit_id: 1)
 c2 = Comment.create(name:"Sean", text: "cool thread", outfit_id: 2)
 c3 = Comment.create(name:"Sekou", text: "basic", outfit_id: 3)
 c4 = Comment.create(name:"AJ", text: "cozy", outfit_id: 1)
 c5 = Comment.create(name:"JSON", text: "favorite slut outfit", outfit_id: 2)
-c6 = Comment.create(name:"Lee", text: "6969 occasion", outfit_id: 3)
-c7 = Comment.create(name:"David", text: "s/o to mama Lee for the care package", outfit_id: 1)
-c8 = Comment.create(name:"Adit", text: "I made some bread!", outfit_id: 2)
+c6 = Comment.create(name:"Lee", text: "6969 occasion", outfit_id: 2)
+c7 = Comment.create(name:"Greg", text: "I like it!", outfit_id: 3)
+c8 = Comment.create(name:"Halton", text: "I'd fk with that", outfit_id: 3)
+c9 = Comment.create(name:"David L.", text: "s/o to mama Lee for the care package", outfit_id: 1)
+c10 = Comment.create(name:"Adit", text: "I had my cereal with this outfit", outfit_id: 2)
+c11 = Comment.create(name:"Jorge", text: "I see you", outfit_id: 1)
+c12 = Comment.create(name:"Lou", text: "Poppin!", outfit_id: 2)
+c13 = Comment.create(name:"Mike", text: "Nice!", outfit_id: 3)
+c14 = Comment.create(name:"David S.", text: "My quarentine outfit", outfit_id: 2)
+c15 = Comment.create(name:"Yoan", text: "I'm hungover", outfit_id: 1)
+c16 = Comment.create(name:"Steven", text: "This is cute! Now buy my table", outfit_id:4)
+c17 = Comment.create(name:"Sean", text: "I need this", outfit_id: 4)
+c18 = Comment.create(name:"Kelly", text: "LOVE IT!", outfit_id: 4)
+
 p "Comments were seeded"
 
 
